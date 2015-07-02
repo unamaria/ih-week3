b38d01817e2630d5a2d27ccb4bf6d3f7ea4d0df3
Maria <unamaria@gmail.com>
2015-07-02 12:27:02 +0200

    Add apple discount

--- day4/shopping_cart/shopping_cart.js
+++ day4/shopping_cart/shopping_cart.js
@@ -10,22 +10,30 @@ Cart.prototype.add = function (amount, item) {
 	} else {
 		this.total += item.price * amount;
 	}
+	console.log('Adding ' + amount + ' to the cart at a price of ' + item.price);
 };
 
 Cart.prototype.calculateTotal = function () {
 	return this.items > 5 ? this.total * 0.9 : this.total;
 };
 
-var Item = function (price, discount) {
+var Item = function (name, price, discount) {
 	this.price = price;
 	this.discount = discount;
 	this.apply_discount = false;
 };
 
-cart = new Cart();
 
-banana = new Item(20, 0.9);
 
-cart.add(3, banana);
+var cart = new Cart();
 
-console.log('Your total is ' + cart.total);
+var banana = new Item(20);
+var orange = new Item(5);
+var apple = new Item(10, 0.5);
+apple.apply_discount = true;
+
+cart.add(1, banana);
+cart.add(2, orange);
+cart.add(2, apple);
+
+console.log('Your total is ' + cart.calculateTotal());
