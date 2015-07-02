0b62700e0b6de737f1fb8a7904bfdf3ab00e0f67
Maria <unamaria@gmail.com>
2015-07-02 13:21:40 +0200

    From scratch (array of items)

--- day4/shopping_cart/shopping_cart.js
+++ day4/shopping_cart/shopping_cart.js
@@ -1,39 +1,56 @@
 var Cart = function () {
 	this.total = 0;
-	this.items = 0;
+	this.items = [];
 };
 
-Cart.prototype.add = function (amount, item) {
-	this.items += amount;
-	if (item.apply_discount) {
-		this.total += item.price * amount * item.discount;
-	} else {
-		this.total += item.price * amount;
+Cart.prototype.add = function (item) {
+	this.applyDiscount(item);
+	console.log('You are adding a ' + item.name + ' at a price of ' + item.price);
+	this.items.push(item);
+};
+
+Cart.prototype.applyDiscount = function (item) {
+	if (item.name === 'orange') {
+		var total_oranges = 0;
+		this.items.forEach (function(item) {
+			total_oranges += item.name === 'orange' ? 1 : 0;
+		});
+
+		if (total_oranges > 4) {
+			item.price = item.price / 2;
+		} 
+
+	} else if (item.name === 'apple') {
+		var apple = new Item('apple', 0);
+		this.items.push(apple);
 	}
-	console.log('Adding ' + amount + ' to the cart at a price of ' + item.price);
 };
 
 Cart.prototype.calculateTotal = function () {
-	return this.items > 5 ? this.total * 0.9 : this.total;
+	this.items.forEach (function(item) {
+		this.total += item.price;
+	}.bind(this));
 };
 
-var Item = function (name, price, discount) {
+var Item = function (name, price) {
+	this.name = name;
 	this.price = price;
-	this.discount = discount;
-	this.apply_discount = false;
 };
 
 
 
 var cart = new Cart();
 
-var banana = new Item(20);
-var orange = new Item(5);
-var apple = new Item(10, 0.5);
-apple.apply_discount = true;
+cart.add(new Item('banana', 20));
+cart.add(new Item('orange', 5));
+cart.add(new Item('orange', 5));
+cart.add(new Item('orange', 5));
+cart.add(new Item('orange', 5));
+cart.add(new Item('orange', 5));
+cart.add(new Item('orange', 5));
+cart.add(new Item('orange', 5));
+cart.add(new Item('apple', 10));
 
-cart.add(1, banana);
-cart.add(2, orange);
-cart.add(2, apple);
+cart.calculateTotal();
 
-console.log('Your total is ' + cart.calculateTotal());
+console.log('Your total is ' + cart.total);
