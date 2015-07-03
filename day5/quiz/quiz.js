1197996c7590ada00c65ee7f3268a210fcac2cb1
Maria <unamaria@gmail.com>
2015-07-03 17:34:52 +0200

    Finish iteration 2

--- day5/quiz/quiz.js
+++ day5/quiz/quiz.js
@@ -21,13 +21,20 @@ Quiz.prototype.addQuestion = function(question) {
 };
 
 Quiz.prototype.firstQuestion = function() {
+	console.log('Hi ' + this.user.name + '!');
+	console.log('Let\'s check how good are you with capitals');
 	this.currentQuestion = this.questions[0];
 	this.getInput();
 };
 
 Quiz.prototype.nextQuestion = function() {
 	if (this.currentQuestionIndex + 1 == this.questions.length) {
-		console.log('Well done!');
+		console.log('You earned a total of ' + this.user.totalPoints + ' points');
+		if (this.user.totalPoints > 0) {
+			console.log('Well done!');
+		} else {
+			console.log('Ehem. Keep playing!')
+		}
 	} else {	
 		this.currentQuestionIndex++;
 		this.currentQuestion = this.questions[this.currentQuestionIndex];
@@ -38,14 +45,17 @@ Quiz.prototype.nextQuestion = function() {
 Quiz.prototype.checkInput = function(err, input) {
 	if (this.currentQuestion.correctAnswer(input)) {
 		console.log('You are good!');
+		this.user.totalPoints += this.currentQuestion.points;
 		this.nextQuestion();
 	} else {
 		console.log('Really?');
+		this.user.totalPoints -= this.currentQuestion.points;
 		this.getInput();
 	}
 };
 
 Quiz.prototype.getInput = function() {
+	console.log('You have ' + this.user.totalPoints + ' points');
 	options = {
 		prompt: this.currentQuestion.print()
 	};
