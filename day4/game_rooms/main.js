1649634419dbaf045e47765bf1811979f7ea6a72
Maria <unamaria@gmail.com>
2015-07-03 09:12:40 +0200

    Add room 3 and 4

--- day4/game_rooms/main.js
+++ day4/game_rooms/main.js
@@ -4,8 +4,12 @@ var Game = require('./game.js');
 
 var game = new Game();
 var room1 = new Room ('You are in room 1. You can go North.', 'Nyanyo', ['N']);
-var room2 = new Room ('You are in room 2. You can go East.', 'Nyanyo', ['E']);
+var room2 = new Room ('You are in room 2. You can go West.', 'Nyanyo', ['W']);
+var room3 = new Room ('You are in room 3. You can go South.', 'Nyanyo', ['S']);
+var room4 = new Room ('You are in room 4. You can go East.', 'Nyanyo', ['E']);
 game.addRoom(room1);
 game.addRoom(room2);
+game.addRoom(room3);
+game.addRoom(room4);
 
 game.start();
